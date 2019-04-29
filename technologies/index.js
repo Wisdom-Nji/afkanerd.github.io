99b881d6fece5ba281914d1ffdf406b8502dee80
Sherlock Wisdom <wisdomnji@gmail.com>
2019-04-29 21:43:56 +0100

    changes

--- technologies/index.js
+++ technologies/index.js
@@ -1,71 +1,65 @@
-window.onload = function() {
-  console.log("loaded");
+var ajax = new XMLHttpRequest;
+ajax.onreadystatechange = function() {
+  if(this.status == 200 && this.readyState == 4) {
+    var sr = JSON.parse(this.responseText);
 
+    for(var repo in sr) {
+      var repo = sr[repo];
+      var kbd = document.createElement("kbd");
+      kbd.appendChild(document.createTextNode(`${repo.name} | ${repo.language}`));
 
+      var h5 = document.createElement("h5");
+      h5.appendChild(kbd);
 
-  var ajax = new XMLHttpRequest;
-  ajax.onreadystatechange = function() {
-    if(this.status == 200 && this.readyState == 4) {
-      var sr = JSON.parse(this.responseText);
+      var samp = document.createElement("samp");
+      samp.appendChild(document.createTextNode(repo.description));
 
-      for(var repo in sr) {
-        var repo = sr[repo];
-        var kbd = document.createElement("kbd");
-        kbd.appendChild(document.createTextNode(`${repo.name} | ${repo.language}`));
+      var p = document.createElement("p");
+      p.setAttribute("style", "text-align: justify");
+      p.appendChild(samp);
 
-        var h5 = document.createElement("h5");
-        h5.appendChild(kbd);
+      var div = document.createElement("div");
+      div.setAttribute("class", "card-body bg-dark text-light");
+      div.appendChild(p);
 
-        var samp = document.createElement("samp");
-        samp.appendChild(document.createTextNode(repo.description));
+      var kbd_ = document.createElement("a");
+      kbd_.setAttribute("style", "cursor: pointer");
+      kbd_.setAttribute("class", "btn btn-secondary");
+      // kbd_.setAttribute("onclick", `window.open(${repo.html_url})`);
+      kbd_.href = repo.html_url;
+      kbd_.appendChild(document.createTextNode("Github"));
 
-        var p = document.createElement("p");
-        p.setAttribute("style", "text-align: justify");
-        p.appendChild(samp);
+      var h5_ = document.createElement("h5");
+      h5_.appendChild(kbd_);
 
-        var div = document.createElement("div");
-        div.setAttribute("class", "card-body bg-dark text-light");
-        div.appendChild(p);
+      var div_ = document.createElement("div");
+      div_.setAttribute("class", "card-footer");
+      div_.appendChild(h5_);
 
-        var kbd_ = document.createElement("a");
-        kbd_.setAttribute("style", "cursor: pointer");
-        kbd_.setAttribute("class", "btn btn-secondary");
-        // kbd_.setAttribute("onclick", `window.open(${repo.html_url})`);
-        kbd_.href = repo.html_url;
-        kbd_.appendChild(document.createTextNode("Github"));
+      var div__ = document.createElement("div");
+      div__.setAttribute("class", "card  bg-dark text-light");
+      div__.appendChild(div);
+      div__.appendChild(div_);
 
-        var h5_ = document.createElement("h5");
-        h5_.appendChild(kbd_);
+      var div___ = document.createElement("div");
+      div___.setAttribute("class", "col-12 text-center");
+      div___.appendChild(h5);
+      div___.appendChild(div__);
 
-        var div_ = document.createElement("div");
-        div_.setAttribute("class", "card-footer");
-        div_.appendChild(h5_);
+      var div____ = document.createElement("div");
+      div____.setAttribute("class", "row");
+      div____.appendChild(div___);
 
-        var div__ = document.createElement("div");
-        div__.setAttribute("class", "card  bg-dark text-light");
-        div__.appendChild(div);
-        div__.appendChild(div_);
+      var br = document.createElement("hr");
 
-        var div___ = document.createElement("div");
-        div___.setAttribute("class", "col-12 text-center");
-        div___.appendChild(h5);
-        div___.appendChild(div__);
+      var main_body = document.getElementById("main_body");
+      main_body.appendChild(div____)
+      main_body.appendChild(br);
 
-        var div____ = document.createElement("div");
-        div____.setAttribute("class", "row");
-        div____.appendChild(div___);
-
-        var br = document.createElement("hr");
-
-        var main_body = document.getElementById("main_body");
-        main_body.appendChild(div____)
-        main_body.appendChild(br);
-
-      }
     }
   }
-
-  ajax.open("GET", "https://api.github.com/orgs/afkanerd/repos");
-  ajax.setRequestHeader("Content-Type", "application/json")
-  ajax.send();
 }
+
+ajax.open("GET", "https://api.github.com/orgs/afkanerd/repos");
+ajax.setRequestHeader("Content-Type", "application/json")
+ajax.send();
