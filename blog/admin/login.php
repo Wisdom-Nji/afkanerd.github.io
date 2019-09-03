124fb6bc84c284a555f58f284cfb44811903bf79
nSedrickm <nsedrick101@gmail.com>
2019-09-03 23:13:19 +0100

    check user login class

--- blog/admin/login.php
+++ blog/admin/login.php
@@ -5,3 +5,32 @@ require_once('../includes/config.php');
 if(!$user -> is_logged_in()) {
   header('Location: login.php');
 }
+
+<form action=""method="post">
+<p><label>Username</label><input type="text" name="username" value=""  /></p>
+<p><label>Password</label><input type="password" name="password" value=""  /></p>
+<p><label></label><input type="submit" name="submit" value="Login"  /></p>
+</form>
+
+//process login form if submitted
+
+if(isset($_POST['submit'])){
+  $username = trim($-POST['username']);
+  $password = trim($_POST['password']);
+
+  if($user -> login($username,$password)){
+    //logged in. return to index page
+    header('Location: index.php');
+    exit;
+
+  }
+  else {
+
+    $message = '<p class="error">Wrong username or password</p>';
+
+  }
+// } end if submit
+
+if(isset($message)) {
+  echo $message;
+}
