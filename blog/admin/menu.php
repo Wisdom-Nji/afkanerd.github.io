ed119873b45f4e0694620128c794dcb8d17d9440
nSedrickm <nsedrick101@gmail.com>
2019-09-04 13:50:44 +0100

    add and edit posts

--- blog/admin/menu.php
+++ blog/admin/menu.php
@@ -5,3 +5,13 @@ require_once('../includes/config.php');
 if(!$user -> is_logged_in()) {
   header('Location: login.php');
 }
+
+<h1>Blog</h1>
+<p>Logged in as <?=$_SESSION['username'];?></p>
+<ul id = 'adminmenu'>
+    <li><a href = 'index.php'>Blog</a></li>
+    <li><a href = 'users.php'>Users</a></li>
+    <li><a href = "../" target = "_blank">View Website</a></li>
+</ul>
+<div class = 'clear'></div>
+<hr />
