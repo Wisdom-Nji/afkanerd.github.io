4d95f077791b2b6ba49ed0e6f69aefae612448b2
nSedrickm <nsedrick101@gmail.com>
2019-09-04 14:26:32 +0100

    add-post and users

--- blog/admin/add-post.php
+++ blog/admin/add-post.php
@@ -5,3 +5,88 @@ require_once('../includes/config.php');
 if(!$user -> is_logged_in()) {
   header('Location: login.php');
 }
+
+<form action = '' method = 'post'>
+
+  <p><label>Title</label><br />
+  <input type ='text' name = 'postTitle' value = '<?php if(isset($error)){echo $_POST['postTitle'];}?>'></p>
+
+  <p><label>Description</label><br />
+  <textarea name='postDesc' cols ='60' rows ='10'><?php if(isset($error)){echo $_POST['postDesc'];}?></textarea></p>
+
+  <p><label>Content</label><br />
+  <textarea name = 'postCont' cols='60' rows ='10'><?php if(isset($error)){echo $_POST['postCont'];}?></textarea></p>
+
+  <p><input type='submit' name = submit' value ='Submit'></p>
+
+  </form>
+
+  <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
+<script>
+        tinymce.init({
+            selector: "textarea",
+            plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        });
+</script>
+
+// if form has been submitted then process it
+if(isset($_POST['submit'])) {
+
+  $_POST = array_map('stripslashes', $_POST);
+
+  //collect form data
+
+  extract($_POST);
+
+  //basic validation for post description ,content and title.
+
+  if($postTitle == '') {
+    $error[] = 'Please enter the post Title.';
+  }
+
+  if($postDesc == '') {
+    $error[] = 'Please enter the post description.';
+  }
+
+  if($postCont == '') {
+    $error[] = 'Please enter post content.';
+  }
+}
+
+ // this will only run if no error occurs
+
+  if(!isset($error)) {
+     try {
+
+       //insert into database
+
+       $stmt = $db -> prepare('INSERT INTO blog_posts (postTitle,postDesc,postCont,postDate) VALUES (:postTitle,:postDesc,:postCont,:postDate)');
+       $stmt -> execute(array(
+         ':postTitle' => $postTitle,
+         ':postDesc'  => $postDesc,
+         ':postCont'  => $postCont,
+         ':postDate'  => date('Y-m-d H:i;s')
+       ));
+
+      //redirect to index page from here
+      header('Location: index.php?action=added');
+      exit;
+
+     }  catch(PDOException $e) {
+        echo $e ->getMessage();
+     }
+
+  }
+
+//if any errors occured then loop through and display
+
+if(isset($error)) {
+  foreach($error as $error) {
+    echo '<p class = "error">'.$error.'</p>';
+  }
+}
--- blog/admin/edit-user.php
+++ blog/admin/edit-user.php
@@ -5,3 +5,80 @@ require_once('../includes/config.php');
 if(!$user -> is_logged_in()) {
   header('Location: login.php');
 }
+
+<?php
+ try {
+
+        $stmt = $db->prepare('SELECT memberID, username, email FROM blog_members WHERE memberID = :memberID') ;
+        $stmt->execute(array(':memberID' => $_GET['id']));
+        $row = $stmt->fetch();
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+?>
+
+<form action='' method='post'>
+    <input type='hidden' name='memberID' value='<?php echo $row['memberID'];?>'>
+
+    <p><label>Username</label><br />
+    <input type='text' name='username' value='<?php echo $row['username'];?>'></p>
+
+    <p><label>Password (only to change)</label><br />
+    <input type='password' name='password' value=''></p>
+
+    <p><label>Confirm Password</label><br />
+    <input type='password' name='passwordConfirm' value=''></p>
+
+    <p><label>Email</label><br />
+    <input type='text' name='email' value='<?php echo $row['email'];?>'></p>
+
+    <p><input type='submit' name='submit' value='Update User'></p>
+
+</form>
+
+
+//When running validation the password checks should only run if a password has been entered.
+//basic validation
+
+if( strlen($password) > 0){
+
+if($password ==''){
+    $error[] = 'Please enter the password.';
+}
+
+if($passwordConfirm ==''){
+    $error[] = 'Please confirm the password.';
+}
+
+if($password != $passwordConfirm){
+    $error[] = 'Passwords do not match.';
+}
+
+}
+
+if(isset($password)){
+
+$hashedpassword = $user->create_hash($password);
+
+//update into database
+$stmt = $db->prepare('UPDATE blog_members SET username = :username, password = :password, email = :email WHERE memberID = :memberID') ;
+$stmt->execute(array(
+    ':username' => $username,
+    ':password' => $hashedpassword,
+    ':email' => $email,
+    ':memberID' => $memberID
+));
+
+
+} else {
+
+//update database
+$stmt = $db->prepare('UPDATE blog_members SET username = :username, email = :email WHERE memberID = :memberID') ;
+$stmt->execute(array(
+    ':username' => $username,
+    ':email' => $email,
+    ':memberID' => $memberID
+));
+
+}
