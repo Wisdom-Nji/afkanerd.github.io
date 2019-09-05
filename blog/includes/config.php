4a52763c934e91fb7bada6e6ac0774bfde96e82c
nSedrickm <nsedrick101@gmail.com>
2019-09-05 09:40:30 +0100

    db.sql

--- blog/includes/config.php
+++ blog/includes/config.php
@@ -3,12 +3,13 @@ ob_start();
 session_start();
 
 //database credentials
-define('DBHOST','localhost');
+define('DBHOST','127.0.0.1:8889');
 define('DBUSER','database username');
 define('DBPASS','database password');
 define('DBNAME','database name');
 
-$db = new PDO("mysql:host=".DBHOST."; port=;dbname=".DBNAME, DBUSER, DBPASS);
+#$db = new PDO("mysql:host=".DBHOST.";port=;dbname=".DBNAME, DBUSER, DBPASS);
+$db = new PDO("mysql:host=".DBHOST.";port=;dbname=".DBNAME, DBUSER, DBPASS);
 $db -> setAttribute(PD0::ATTR_ERRMODE, PD0::ERRMODE_EXCEPTION);
 
 //set timezone
